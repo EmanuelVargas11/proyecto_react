683abee98b47ded063682e574613bc9df8e85faf
EmanuelVargas11 <vargasriquelmege@gmail.com>
2025-03-02 21:19:13 -0300

    Se inicio el proyecto y se realizo un componente Titulo.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import Titulo from './Titulo'
+function App() {
+
+  return (
+    <>
+      <Titulo/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Titulo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Titulo = () => {
+  return (
+    <h1 className='text-center bg-primary text-white p-2'>Proyecto React</h1>
+  )
+}
+
+export default Titulo
